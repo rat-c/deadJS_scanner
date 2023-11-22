0760b3571cab6dc9060f453252650d591a8500ff
ratcode404 <89196953+ratcode404@users.noreply.github.com>
2023-11-22 14:37:57 +0100

    Delete background.js

--- background.js
+++
@@ -1,9 +0,0 @@
-chrome.webRequest.onCompleted.addListener(
-    function(details) {
-        if (details.type === "script" && details.statusCode !== 200) {
-            console.error("Script failed to load:", details.url);
-            // Add your notification code here
-        }
-    },
-    { urls: ["<all_urls>"] }
-);
